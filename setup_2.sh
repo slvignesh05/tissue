553ae05169d89e5988d4aec667c8c074a0be0734
slvignesh05 <123097688+slvignesh05@users.noreply.github.com>
2025-08-29 18:03:46 +0530

    Update setup_2.sh

--- setup_2.sh
+++ setup_2.sh
@@ -1,42 +1,28 @@
 #!/bin/bash
-# setup_2.sh - Install recon and scanning tools
+# setup_2.sh - Install recon and scanning tools (fixed)
 
 set -e
 
 echo "[*] Installing dependencies..."
-sudo apt update && sudo apt install -y golang-go git wget python3 python3-pip
+sudo apt update && sudo apt install -y golang-go git wget python3 python3-pip dirb
 
 TOOLS_DIR="$HOME/recon_tools"
 mkdir -p $TOOLS_DIR && cd $TOOLS_DIR
 
-# Install GoBuster
-echo "[*] Installing GoBuster..."
-go install github.com/OJ/gobuster/v3@latest
-ln -sf ~/go/bin/gobuster /usr/local/bin/gobuster
+install_tool() {
+    TOOL=$1
+    CMD=$2
+    BIN=$3
+    echo "[*] Installing $TOOL..."
+    go install $CMD
+    sudo ln -sf ~/go/bin/$BIN /usr/local/bin/$BIN
+}
 
-# Install GoSpider
-echo "[*] Installing GoSpider..."
-go install github.com/jaeles-project/gospider@latest
-ln -sf ~/go/bin/gospider /usr/local/bin/gospider
+install_tool "GoBuster" "github.com/OJ/gobuster/v3@latest" "gobuster"
+install_tool "GoSpider" "github.com/jaeles-project/gospider@latest" "gospider"
+install_tool "KiteRunner" "github.com/assetnote/kiterunner@latest" "kr"
+install_tool "Waybackurls" "github.com/tomnomnom/waybackurls@latest" "waybackurls"
+install_tool "CRLFuzz" "github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest" "crlfuzz"
+install_tool "Nuclei" "github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest" "nuclei"
 
-# Install KiteRunner
-echo "[*] Installing KiteRunner..."
-go install github.com/assetnote/kiterunner@latest
-ln -sf ~/go/bin/kr /usr/local/bin/kr
-
-# Install Waybackurls
-echo "[*] Installing Waybackurls..."
-go install github.com/tomnomnom/waybackurls@latest
-ln -sf ~/go/bin/waybackurls /usr/local/bin/waybackurls
-
-# Install CRLFuzz
-echo "[*] Installing CRLFuzz..."
-go install github.com/dwisiswant0/crlfuzz/cmd/crlfuzz@latest
-ln -sf ~/go/bin/crlfuzz /usr/local/bin/crlfuzz
-
-# Install Nuclei
-echo "[*] Installing Nuclei..."
-go install github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
-ln -sf ~/go/bin/nuclei /usr/local/bin/nuclei
-
-echo "[+] All tools installed successfully in $TOOLS_DIR"
+echo "[+] All tools installed successfully!"
